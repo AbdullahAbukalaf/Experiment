d32df29c07d5b49b0b65b934f25b26ca2f9e1e4c
Abdullah <abdullahazzamabukhalaf@gmail.com>
2024-08-31 23:44:19 +0300

    adding a collision file that hold collision and many more methods

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     // speed at which the object moves
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 18f;
     // rotation speed at which the object rotates
-    [SerializeField] float rotateSpeed = 1f;
+    [SerializeField] float rotateSpeed = 300f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerSpeed = Input.GetAxis("Horizontal") * rotateSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerSpeed = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerSpeed);
         transform.Translate(0 , moveAmount, 0);
     }
